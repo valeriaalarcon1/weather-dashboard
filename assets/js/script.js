94875110dcb0f1587565533c8968d39e3750d23a
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-10-11 20:59:06 -0400

    Working on JS code for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var temp = document.getElementById('temp');
 var wind = document.getElementById('wind');
 var hum = document.getElementById('hum');
 // City 5-Day Forecast Container
-var cityForecast = document.getElementById('forecast-name');
+var cityForecast = document.getElementById('forecast-container');
 // API variables
 var city;
 var APIKey = "b0bc992715f4e9f7987f6f966de30308";
@@ -26,7 +26,7 @@ searchBtn.on('click', function() {
         return;
     }
     else {
-        getAPI();
+    getAPI();
     }
 
 })
@@ -50,7 +50,36 @@ function getAPI() {
     temp.textContent = "Temp:  " + data.main.temp + "ºF";
     wind.textContent = "Wind:  " + data.wind.speed + " mph";
     hum.textContent = "Humidity:  " + data.main.humidity + "%";
-    
-        
+
+
+    var city = document.getElementById('userInput').value;
+    var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    fetch(forecastURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data) 
+
+    })
+
+    for (var i=4; i < data.length; i+16) {
+        var dayDiv = document.createElement('div');
+        var dayTemp = document.createElement('p');
+        var dayWind = document.createElement('p');
+        var dayHum = document.createElement('p');
+
+        dayTemp.textContent = "Temp:  " + data[i].main.temp + "ºF";
+        dayWind.textContent = "Wind:  " + data[i].wind.speed + " mph";
+        dayHum.textContent = "Humidity:  " + data[i].main.humidity + "%";
+
+        dayDiv.appendChild(dayTemp);
+        dayDiv.appendChild(dayWind);
+        dayDiv.appendChild(dayHum);
+        forecastContainer.appendChild(dayDiv);
+
+        console.log(data[4].main.temp);
+    }
+
     })
 }
